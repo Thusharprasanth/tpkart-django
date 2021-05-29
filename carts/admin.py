2404784d5891a8bd210b6cbdfc4c11ec3a9c1ad1
thushar prasanth <thusharprasanth@gmail.com>
2021-05-29 23:19:27 +0530

    created store app

---
+++ carts/admin.py
@@ -0,0 +1,7 @@
+from carts.models import CartItem
+from django.contrib import admin
+from .models import Cart, CartItem
+
+# Register your models here.
+admin.site.register(Cart)
+admin.site.register(CartItem)
