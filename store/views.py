6c0e10ed4e7fe245f193dfbf173db84437200ff4
thushar prasanth <thusharprasanth@gmail.com>
2021-06-01 12:11:45 +0530

    search function completed

--- store/views.py
+++ store/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from  carts.views import _cart_id
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -19,7 +20,7 @@ def store(request, category_slug=None):
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         product_count = products.count()
         paginator = Paginator(products, 3)
         page = request.GET.get('page')
@@ -42,3 +43,16 @@ def product_details(request, category_slug, product_slug):
         'in_cart' : in_cart
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains = keyword))
+            product_count = products.count()
+    context = {
+            'products' : products,
+            'product_count' : product_count,
+        }
+
+    return render(request, 'store/store.html', context)
